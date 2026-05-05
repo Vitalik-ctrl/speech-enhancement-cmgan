4c28cbcb8c3e9be1321ce0c117560d04c0035bb6
Vitalii Varhanik <58106424+Vitalik-ctrl@users.noreply.github.com>
2026-05-05 14:20:03 +0200

    Modify checkpoint handling and config file used
    Updated checkpoint rescue and configuration file paths.

--- templates/hpc/metacentrum_template.sh
+++ templates/hpc/metacentrum_template.sh
@@ -33,9 +33,6 @@ test -n "${SCRATCHDIR:-}" || { echo >&2 "SCRATCHDIR not set"; exit 1; }
 
 ls /auto/projects-du-praha/CTU_Speech_Lab > /dev/null 2>&1 || true
 
-# the problem with paths, some frontends have auto/ some /projects/ and some /projects-du-praha/
-# consult metacentrum.cz
-
 CANDIDATES=(
   "/storage/projects-du-praha/CTU_Speech_Lab/workspace/varhavit/speech-enhancement-cmgan"
   "/storage/projects/CTU_Speech_Lab/workspace/varhavit/speech-enhancement-cmgan"
@@ -65,14 +62,14 @@ test -x "$PYTHON_EXEC" || { echo >&2 "Python not found/executable: $PYTHON_EXEC"
 export SAVE_DIR="$SCRATCHDIR/checkpoints"
 mkdir -p "$SAVE_DIR"
 
-FINAL_CHECKPOINT_DIR="$PBS_O_HOME/cmgan_checkpoints/${PBS_JOBID}"
+FINAL_CHECKPOINT_DIR="$WORKSPACE_DIR/rescued_checkpoints/${PBS_JOBID}"
 mkdir -p "$FINAL_CHECKPOINT_DIR"
 
 rescue_checkpoints() {
     echo "=================================================="
     echo "Job ending or Walltime reached! Rescuing checkpoints..."
 
-    cp -r "$SAVE_DIR/"* "$FINAL_CHECKPOINT_DIR/" || echo "WARNING: Copy failed or no files to copy!"
+    cp -a "$SAVE_DIR/." "$FINAL_CHECKPOINT_DIR/" || echo "WARNING: Copy failed!"
 
     echo "Checkpoints successfully copied to $FINAL_CHECKPOINT_DIR"
     echo "Cleaning local scratch..."
@@ -88,11 +85,15 @@ sed \
   -e "s|^  workspace:.*|  workspace: \"$WORKSPACE_DIR\"|g" \
   -e "s|^  scratch:.*|  scratch: \"$SCRATCHDIR\"|g" \
   -e "s|^  save_dir:.*|  save_dir: \"$SAVE_DIR\"|g" \
-  config/metacentrum.yaml > "$RUNTIME_CFG"
+  config/training_mixed_data_short.yaml > "$RUNTIME_CFG"
 
 echo "Runtime config written to: $RUNTIME_CFG"
 
 export PYTHONPATH="$WORKSPACE_DIR:$PROJECT_DIR/src${PYTHONPATH:+:$PYTHONPATH}"
+
+# Set PyTorch CUDA memory allocator to use expandable segments, which can help with fragmentation issues on long-running jobs
+# [For me] Check $HOME/pbs_logs/cmgan/pbs_17583979.pbs-m1.metacentrum.cz.log for more information
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 echo "Starting CMGAN Training..."
 
